560b81709f800142411afcc156ac9a3fdfb69a38
Tom Kent <email@tomkent.me>
2015-07-15 10:17:24 +0100

    Fixed GenHelper to have correct "Item" sprite directory

--- Generators/laravel/app/config/gen.php
+++ Generators/laravel/app/config/gen.php
@@ -5,7 +5,7 @@ return array(
 	'langProjectDir' => dirname(dirname(dirname(app_path()))).DIRECTORY_SEPARATOR."Development".DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."main".DIRECTORY_SEPARATOR."resources".DIRECTORY_SEPARATOR."assets".DIRECTORY_SEPARATOR."chemcraft".DIRECTORY_SEPARATOR."lang".DIRECTORY_SEPARATOR,
 	'langDir' => dirname(dirname(dirname(app_path()))).DIRECTORY_SEPARATOR."Language".DIRECTORY_SEPARATOR,
 	'spritesDir' => dirname(dirname(dirname(app_path()))).DIRECTORY_SEPARATOR."Sprites".DIRECTORY_SEPARATOR,
-	'spritesDirItems' => dirname(dirname(dirname(app_path()))).DIRECTORY_SEPARATOR."Sprites".DIRECTORY_SEPARATOR."items".DIRECTORY_SEPARATOR,
+	'spritesDirItems' => dirname(dirname(dirname(app_path()))).DIRECTORY_SEPARATOR."Sprites".DIRECTORY_SEPARATOR."Items".DIRECTORY_SEPARATOR,
 	'codeDir' => dirname(dirname(dirname(app_path()))).DIRECTORY_SEPARATOR."Development".DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."main".DIRECTORY_SEPARATOR."java".DIRECTORY_SEPARATOR."me".DIRECTORY_SEPARATOR."tomkent".DIRECTORY_SEPARATOR."chemcraft".DIRECTORY_SEPARATOR,
 	'projectTextureDir' => dirname(dirname(dirname(app_path()))).DIRECTORY_SEPARATOR."Development".DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."main".DIRECTORY_SEPARATOR."java".DIRECTORY_SEPARATOR."assets".DIRECTORY_SEPARATOR."chemcraft".DIRECTORY_SEPARATOR."textures".DIRECTORY_SEPARATOR,
 	'projectAssetDir' => dirname(dirname(dirname(app_path()))).DIRECTORY_SEPARATOR."Development".DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."main".DIRECTORY_SEPARATOR."java".DIRECTORY_SEPARATOR."assets".DIRECTORY_SEPARATOR."chemcraft".DIRECTORY_SEPARATOR,
